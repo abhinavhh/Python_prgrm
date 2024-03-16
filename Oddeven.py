71705a45b84a168dbfc0f616f41b42c6471cced0
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-16 19:02:15 +0530

    Delete Oddeven.py

--- Oddeven.py
+++
@@ -1,6 +0,0 @@
-print("First python program for finding a number is ogg or even")
-a=int(input("Enter a number : "))
-if a % 2 == 0:
-  print("Number "+a +"is even")
-else:
-  print("Number"+a +"is odd")
