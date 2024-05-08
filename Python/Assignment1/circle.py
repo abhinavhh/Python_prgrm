9a99b3eb6abbb5eb3c12ca508a57b66a26be7bb8
Abhinav U <129955044+abhinavhh@users.noreply.github.com>
2024-05-08 14:13:46 +0530

    Update circle.py

--- Python/Assignment1/circle.py
+++ Python/Assignment1/circle.py
@@ -11,3 +11,8 @@ print("Area of circle is : ",area)
 print("Circumference of the circle is : ",circumference)
 
 print()
+
+Output:
+  Enter the radius of the circle : 3
+  Area of circle is :  28.274333882308138
+  Circumference of the circle is :  18.84955592153876
